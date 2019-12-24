7ccc8b0258c34872aeac7bca0795c83b336364a6
xiaomu <lilin@gmail.com>
2019-12-24 22:50:07 +0800

    initial commit

---
+++ app/src/main/java/com/wjf/beacontower/TowerApplication.java
@@ -0,0 +1,12 @@
+package com.wjf.beacontower;
+
+import android.app.Application;
+
+public class TowerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
